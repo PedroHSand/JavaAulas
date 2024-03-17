9a08b81594faed64c9aa2f4f366a91239d136696
PedroHSand <132959382+PedroHSand@users.noreply.github.com>
2024-03-17 17:09:04 -0300

    ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,23 +1,13 @@
-public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package contabanco;
-
 import java.util.Scanner;
 
 public class ContaTerminal {
+    
     public int numero;
     public String agencia;
     public String nomeCliente;
     public double saldo;
-
-    public static void main(String[] args) {
+    
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         ContaTerminal conta = new ContaTerminal();
 
@@ -37,3 +27,4 @@ public class ContaTerminal {
         System.out.println("Olá " + conta.nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + conta.agencia + ", conta " + conta.numero + " e seu saldo de R$" + conta.saldo + " já está disponível para saque.");
     }
 }
+
